7ed3aecba41e7dcf26c29ab78a532c983972cb8a
andrewsimmons <neon.duncan@gmail.com>
2021-10-18 16:20:35 -0500

    set up basic jwt auth in user controller and auth controller and application controller, installed figaro and reacted applicaiton.yaml, added config/application.yaml to .gitignore

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def create
-        user = User.new(user_params)
-        if user.save
-            render json:user, status: :created
+        @user = User.new(user_params)
+        if @user.save
+            render json:@user, status: :created
         else
-            render json: {errors: user.errors.full_messages}, status: 422
+            render json: {errors: @user.errors.full_messages}, status: 422
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :transactions, only: [:create, :update, :destroy]
-  resources :budgets
-  resources :users, only: [:create, :show]
+  namespace :api do 
+    namespace :v1 do
+      resources :transactions, only: [:create, :update, :destroy]
+      resources :budgets
+      resources :users, only: [:create, :show]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
