b46bd9c82fd206b354cf0cf2fd915ca0aa7c5419
andrewsimmons <neon.duncan@gmail.com>
2021-10-28 11:09:19 -0500

    changed cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4000'
+    # origins 'localhost:4000'
+    origins 'https://damp-brushlands-95494.herokuapp.com/'
 
     resource '*',
       headers: :any,
