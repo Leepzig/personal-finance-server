2b470eb23420f6564bf50deec3d68f344af539d9
andrewsimmons <neon.duncan@gmail.com>
2021-11-16 09:32:30 -0600

    adjusted orgins in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://damp-brushlands-95494.herokuapp.com/api/v1'
+    origins 'https://damp-brushlands-95494.herokuapp.com'
     # origins 'http://localhost:4000'
 
     resource '*',
