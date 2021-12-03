c988f1cf2c8d38acd8739e9275162fa02a792d03
andrewsimmons <neon.duncan@gmail.com>
2021-12-03 10:43:51 -0600

    changed the settings for developement on localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://damp-brushlands-95494.herokuapp.com'
-    # origins 'http://localhost:4000'
+    # origins 'https://damp-brushlands-95494.herokuapp.com'
+    origins 'http://localhost:4000'
 
     resource '*',
       headers: :any,
